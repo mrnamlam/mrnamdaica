38a03bcea97332b4f871d29fdbf9111d5dd53c73
Lâm Nhật Nam <128274881+mrnamlam@users.noreply.github.com>
2023-07-12 16:07:06 +0700

    Code Mathlab

---
+++ Ex_02.m
@@ -0,0 +1,4 @@
+% Tính điện trở của mạch song song 
+R1 = 100 %ohm
+R2 = 200 %ohm
+R = 1 / (1/R1 + 1/R2)
---
+++ Ex_03.m
@@ -0,0 +1,9 @@
+% tính cơ năng của một vật 
+% cơ năng = động năng + thế năng
+m = 0.1
+g = 9.81
+h = 1
+v = 5
+D = (1/2)*m*v^2
+T = m*g*h
+E = D + T
